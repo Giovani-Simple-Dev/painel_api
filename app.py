2e05ee8e218b26bfe81f71c1ab4b2988bcd66b8a
Giovani-Simple-Dev <167205762+Giovani-Simple-Dev@users.noreply.github.com>
2024-05-30 01:01:35 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def consulta_cpf():
     dados = ler_dados_json()
     for row in dados:
         if row['cpf'] == cpf:
-            return jsonify({'cpf': row['cpf'], 'nome': row['nome'], 'idade': row['idade'], 'data_nascimento': row['data_nascimento'], 'sexo': row['sexo'], 'escolaridade': row['escolaridade'], 'nome_mae': row['nome_mae'], 'nome_pai': row['nome_pai'], 'parentes': row['parentes'], 'telefones': row['telefones']})
+            return jsonify({'cpf': row['cpf'], 'nome': row['nome'], 'data_nascimento': row['data_nascimento'], 'sexo': row['sexo'], 'escolaridade': row['escolaridade'], 'mae': row['mae'], 'pai': row['pai'], 'parentes': row['parentes'], 'telefones': row['telefones'], 'situacao_cadastral': row['situacao_cadastral'], 'cns': row['cns'], 'obito': row['obito'], 'data_obito': row['data_obito'], 'titulo_eleitor': row['titulo_eleitor'], 'numero_rg': row['numero_rg'], 'orgao_emissor': row['orgao_emissor'], 'uf': row['uf'], 'pis': row['pis'], 'renda': row['renda'], 'poder_aquisitivo': row['poder_aquisitivo'], 'faixa_aquisitiva': row['faixa_aquisitiva'], 'scorecsba': row['scorecsba'], 'scorecsb': row['scorecsb'], 'telefones': row['telefones'], 'emails': row['emails'], 'cep': row['cep'], 'estado': row['estado'], 'municipio': row['municipio'], 'bairro': row['bairro'], 'logradouro': row['logradouro'], 'tipo': row['tipo'], 'numero': row['numero']})
 
     return jsonify({'error': 'CPF não encontrado'}), 404
 
